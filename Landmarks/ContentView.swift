10ba4df1f8fb6d8b7c13a2124f8cbdce5c3e7116
George Mapaya <lags-twisted-0b@icloud.com>
2022-06-15 09:26:19 -0400

    Completed SwiftUI Essentials: Creating and Combining Views

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,18 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-                .foregroundColor(.green)
-            HStack {
-                Text("Joshua Tree National Park")
-                Spacer()
-                Text("California")
+        VStack {
+            Mapview()
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundColor(.green)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
-            .font(.subheadline)
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
---
+++ Landmarks/Mapview.swift
@@ -0,0 +1,23 @@
+//
+//  Mapview.swift
+//  Landmarks
+//
+//  Created by George Mapaya on 2022-06-15.
+//
+
+import MapKit
+import SwiftUI
+
+struct Mapview: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct Mapview_Previews: PreviewProvider {
+    static var previews: some View {
+        Mapview()
+    }
+}
