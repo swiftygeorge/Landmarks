051984573fef6800b7ebaeeb9124f3d404096319
George Mapaya <lags-twisted-0b@icloud.com>
2022-06-16 06:42:14 -0400

    Completed Drawing and Animation: Animation Views and Transitions

---
+++ Landmarks/Model/Hike.swift
@@ -0,0 +1,30 @@
+//
+//  Hike.swift
+//  Landmarks
+//
+//  Created by George Mapaya on 2022-06-16.
+//
+
+import Foundation
+
+struct Hike: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var distance: Double
+    var difficulty: Int
+    var observations: [Observation]
+    
+    static var formatter = LengthFormatter()
+    
+    var distanceText: String {
+        Hike.formatter
+            .string(fromValue: distance, unit: .kilometer)
+    }
+    
+    struct Observation: Codable, Hashable {
+        var distanceFromStart: Double
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
+    }
+}
