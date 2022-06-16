4bc3a40d6a0f05460fa1798574e8b4a13cd685ae
George Mapaya <lags-twisted-0b@icloud.com>
2022-06-16 09:40:48 -0400

    Completed App Design and Layout: Working with UI Controls

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
+    @State private var showingProfile = false
     
     var body: some View {
         NavigationStack {
@@ -25,7 +26,21 @@ struct CategoryHome: View {
                 }
                 .listRowInsets(EdgeInsets())
             }
+            .listStyle(.inset)
             .navigationTitle("Featured")
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        showingProfile.toggle()
+                    } label: {
+                        Label("User Profile", systemImage: "person.crop.circle")
+                    }
+                    .sheet(isPresented: $showingProfile) {
+                        ProfileHost()
+                            .environmentObject(ModelData())
+                    }
+                }
+            }
         }
     }
 }
